7be58cd008e4680aa2488c45e541d5531d0074bc
song <ssb00161@naver.com>
2023-09-18 16:54:42 +0900

    when elevator boarding or getting off, ignore moving sound

--- src/speaker.py
+++ src/speaker.py
@@ -22,6 +22,7 @@ class Speaker:
         # Define the sound file paths
         self.sound_path = package_path + "/sounds/"
         self.isPrioritySoundPlaying = False
+        self.playingSound="none"
 
 
     def playSoundCallback(self, data):
@@ -32,15 +33,22 @@ class Speaker:
 
 
         if data.data == "moving":
+            if self.playingSound == "elevatorBoarding" or self.playingSound == "elevatorGettingOff":
+                return
+            
             self.isPrioritySoundPlaying = False
+            self.playingSound = "moving"
             self.playEffectSound('moving.wav', -1)
             
         elif data.data == "emergencyStop":
             self.isPrioritySoundPlaying = False
+            self.playingSound = "emergencyStop"
             self.playEffectSound('emo.wav', -1)
 
         elif data.data == "robotError":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "robotError"
+            
             self.playEffectSound('alarm.mp3', 1)
             
             while mixer.music.get_busy():
@@ -50,6 +58,8 @@ class Speaker:
 
         elif data.data == "lowBattery":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "lowBattery"
+            
             self.playEffectSound('alarm.mp3', 1)
             
             while mixer.music.get_busy():
@@ -59,6 +69,8 @@ class Speaker:
             
         elif data.data == "cannotMove":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "cannotMove"
+
             self.playEffectSound('warning.mp3', 1)
             
             while mixer.music.get_busy():
@@ -68,10 +80,14 @@ class Speaker:
 
         elif data.data == "obstacle":
             self.isPrioritySoundPlaying = False
+            self.playingSound = "obstacle"
+
             self.playEffectSound('obstacle.wav', 1)
 
         elif data.data == "deliveryCancel":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "deliveryCancel"
+
             self.playEffectSound('cancel.mp3', 1)
             
             while mixer.music.get_busy():
@@ -81,10 +97,14 @@ class Speaker:
 
         elif data.data == "deliveryPause":
             self.isPrioritySoundPlaying = False
+            self.playingSound = "deliveryPause"
+
             self.playEffectSound('pause.wav', -1)
 
         elif data.data == "deliveryReady":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "deliveryReady"
+
             self.playVoiceSound('_voice_deliveryReady.mp3', 1)       
 
             while mixer.music.get_busy():
@@ -94,6 +114,8 @@ class Speaker:
 
         elif data.data == "elevatorBoarding":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "elevatorBoarding"
+            
             self.playVoiceSound('_voice_elevatorBoarding.mp3', 1)
             
             while mixer.music.get_busy():
@@ -103,6 +125,8 @@ class Speaker:
 
         elif data.data == "elevatorGettingOff":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "elevatorGettingOff"
+
             self.playVoiceSound('_voice_elevatorGettingOff.mp3', 1)
             
             while mixer.music.get_busy():
@@ -112,6 +136,8 @@ class Speaker:
 
         elif data.data == "deliveryArrival":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "deliveryArrival"
+
             self.playEffectSound('arrived.wav', 1)
             
             while mixer.music.get_busy():
@@ -121,6 +147,8 @@ class Speaker:
 
         elif data.data == "deliveryPickupRequest":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "deliveryPickupRequest"
+            
             self.playVoiceSound('_voice_deliveryPickupRequest.mp3', 1)
             
             while mixer.music.get_busy():
@@ -130,6 +158,8 @@ class Speaker:
 
         elif data.data == "returnStart":
             self.isPrioritySoundPlaying = True
+            self.playingSound = "returnStart"
+
             self.playEffectSound('finish.wav', 1)
             
             while mixer.music.get_busy():
@@ -138,6 +168,7 @@ class Speaker:
             self.playVoiceSound('_voice_returnStart.mp3', 1)
 
         elif data.data =="soundOff":
+            self.playingSound = "soundOff"
             mixer.music.stop()
 
         elif data.data =="DISCONNECTED":
